f0f7ef22b2ea216660d0f1fc0f1f0384405055be
WebberWu <bird23074035@gmail.com>
2016-07-29 18:34:56 +0800

    內頁第一個階段完成

--- mobile_web/public/dist/js/main.js
+++ mobile_web/public/dist/js/main.js
@@ -7,4 +7,10 @@ $(function(){
         adaptiveHeight: true,
         arrows: false
     });
+    // 文章文字大小調整
+    $('.font-size-controllers .font-link').on('click', function(){
+        var fontClass = $(this).attr('class').match(/font-size-[a-z]+/g);
+        var beforeClass = $('.news-body').attr('class').match(/font-size-[a-z]+/g) || [];
+        $('.news-body').removeClass(beforeClass[0]).addClass(fontClass[0]);
+    });
 });
