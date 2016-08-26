0c351d75f7f78b9e43699e17dac3c8734f27a886
esbb48 <esbb48@gmail.com>
2016-08-26 17:37:47 +0800

    Add video one page

--- mobile_web/controllers/video/index.js
+++ mobile_web/controllers/video/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 let router = express.Router();
 
 import listByCategory from './listByCategory';
+import one from './one';
 
 router.route('/video')
     .get(listByCategory);
@@ -9,5 +10,7 @@ router.route('/video')
 router.route('/video/category/:taxId')
     .get(listByCategory);
 
+router.route('/video/:videoId')
+    .get(one);
 
 module.exports = router;
---
+++ mobile_web/controllers/video/one.js
@@ -0,0 +1,29 @@
+import co from 'co';
+import express from 'express';
+import getApi from '../../util/getApi';
+
+let router = express.Router();
+
+const debug = require('debug')('NOWmobile:controllers:video');
+
+module.exports = (req, res, next) => {
+    let { videoId } = req.params;
+
+    if (!videoId) return next();
+
+    co(function*() {
+
+        let video = yield getApi(`videos/${videoId}`);
+        let headline = yield getApi(`news/headline`);
+
+        if(req.query.data === 'PLAYJJ'){
+            return res.json({ video });
+        }
+
+        return res.render('video/one', {
+            video,
+            headline,
+        });
+
+    }).catch(next);
+};
