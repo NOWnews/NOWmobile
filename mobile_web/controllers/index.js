82980d4e467a1288f8a202b119bf42f3a2a6edea
Simon Sun <blackghost0118@gmail.com>
2016-08-30 21:27:34 +0800

    增加 robots 端點，並且增加未來要輸出 file 的端點

--- mobile_web/controllers/index.js
+++ mobile_web/controllers/index.js
@@ -3,6 +3,7 @@ import home from './home';
 import news from './news';
 import photo from './photo';
 import video from './video';
+import files from './files';
 
 
 module.exports = function(app) {
@@ -12,6 +13,7 @@ module.exports = function(app) {
     app.use('/', news);
     app.use('/', photo);
     app.use('/', video);
+    app.use('/', files);
 
     return function(req, res, next) {
         return next();
