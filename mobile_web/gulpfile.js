d544d3757c16fa5baab9cfc865b92c3c3ec4bb3a
esbb48 <esbb48@gmail.com>
2016-09-01 19:27:22 +0800

    Refactor path

--- mobile_web/gulpfile.js
+++ mobile_web/gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const del = require('del');
 const path = require('path');
+const cssPath = path.join('./public/dist/css');
+const jsPath = path.join('./public/dist/js');
+console.log(cssPath);
 const plugins = require('gulp-load-plugins')({
   pattern: ['gulp-*', 'gulp.*'],
   replaceString: /\bgulp[\-.]/
@@ -9,31 +12,31 @@ const plugins = require('gulp-load-plugins')({
 // 編譯 css
 gulp.task('css', function() {
   return gulp.src([
-    './public/dist/css/font-awesome.min.css',
-    './public/dist/css/slick.min.css',
-    './public/dist/css/slick-theme.min.css',
-    './public/dist/css/mui.min.css',
-    './public/dist/css/main.css',
+    `${cssPath}/font-awesome.min.css`,
+    `${cssPath}/slick.min.css`,
+    `${cssPath}/slick-theme.min.css`,
+    `${cssPath}/mui.min.css`,
+    `${cssPath}/main.css`,
   ]).pipe(plugins.plumber())
     .pipe(plugins.concatCss('all.min.css'))
     .pipe(plugins.cleanCss())
     .pipe(plugins.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('./public/dist/css'));
+    .pipe(gulp.dest(cssPath));
 });
 
 // 將 js 編譯成 minify
 gulp.task('script', function() {
   return gulp.src([
-    './public/dist/js/jquery-2.2.4.min.js',
-    './public/dist/js/slick.min.js',
-    './public/dist/js/mui.min.js',
-    './public/dist/js/main.js',
+    `${jsPath}/jquery-2.2.4.min.js`,
+    `${jsPath}/slick.min.js`,
+    `${jsPath}/mui.min.js`,
+    `${jsPath}/main.js`,
   ]).pipe(plugins.plumber())
     .pipe(plugins.concat('all.min.js'))
     .pipe(plugins.uglify())
-    .pipe(gulp.dest('./public/dist/js'));
+    .pipe(gulp.dest(jsPath));
 });
 
 // 清掉 dist 裡面 css 跟 js 的資料夾
