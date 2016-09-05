75659f8cc45089dee1226f98ad4b831633f2330b
esbb48 <esbb48@gmail.com>
2016-09-05 14:45:23 +0800

    First version

--- mobile_web/source/js/main.js
+++ mobile_web/source/js/main.js
@@ -63,8 +63,7 @@ $(function() {
 
     // 開關 channel nav
     $('#channel-nav-block > .mui-panel').on('click', function() {
-        $('#channel-nav').toggleClass('mui--hide');
-        $('#channel-content').toggleClass('mui--hide');
+        $('#channel-nav-block').toggleClass("is-open");
         $(this).find('.channel-name').toggleClass('mui--hide');
         $(this).find('i').toggleClass('fa-rotate-270');
         return;
