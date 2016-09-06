d2dd68a91c90c284fdef8e1faf3116b2ae963b2c
esbb48 <esbb48@gmail.com>
2016-09-06 13:36:50 +0800

    hidden social link

--- mobile_web/source/js/main.js
+++ mobile_web/source/js/main.js
@@ -117,16 +117,16 @@ $(function() {
         navCategoryCenter();
     }
 
-    // 內頁的 social
-    $( window ).load(function() {
-        // social 在 safari 會出現 img, 所以要隱藏起來
-        $('img[src="http://load.s3.amazonaws.com/pixel.gif"]').hide();
-    });
-    var socialDom = $('#social-link').length > 0;
-    if (socialDom) {
-        var socialHeight = 40;
-        $('.custom-space').css('height', socialHeight);
-    }
+    // 原內頁置底的 social
+    // $( window ).load(function() {
+    //     // social 在 safari 會出現 img, 所以要隱藏起來
+    //     $('img[src="http://load.s3.amazonaws.com/pixel.gif"]').hide();
+    // });
+    // var socialDom = $('#social-link').length > 0;
+    // if (socialDom) {
+    //     var socialHeight = 40;
+    //     $('.custom-space').css('height', socialHeight);
+    // }
 
     // 滑到底去抓新聞
     var win = $(window);
