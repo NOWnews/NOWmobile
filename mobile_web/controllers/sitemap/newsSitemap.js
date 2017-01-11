c4ecaa7b7f52a0ec8a2fbfb4cd5f0b7cca53f5cd
Simon Sun <blackghost0118@gmail.com>
2017-01-11 13:03:36 +0800

    拼出 mobile web news sitemap

---
+++ mobile_web/controllers/sitemap/newsSitemap.js
@@ -0,0 +1,65 @@
+
+import sm from 'sitemap';
+import co from 'co';
+import Promise from 'bluebird';
+import _ from 'lodash';
+
+import getApi from '../../util/getApi';
+
+// 違法字元轉換成正確 xml 字源格式對照表
+const entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+};
+
+// 替換掉違法字元的 function 
+const escapeHtml = function (string) {
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+        return entityMap[s];
+    });
+};
+
+module.exports = (req, res, next) => {
+
+    co(function*() {
+
+        // 從 api 取得 sitemap 的資料
+        let sitemapData = yield getApi('sitemap/newsSitemap');
+
+
+        if(!sitemapData || sitemapData.length === 0) {
+            return yield Promise.reject(new Error('sitemap api 找不到資料.....'));
+        }
+
+        let xmlContents = _.map(sitemapData, (data) => {
+            return `
+                <url>
+                    <loc>${data.url}</loc>
+                    <news:news>
+                        <news:publication>
+                            <news:name>${data.name}</news:name>
+                            <news:language>${data.language}</news:language>
+                        </news:publication>
+                        <news:genres>${data.genres}</news:genres>
+                        <news:publication_date>${data.publication_date}</news:publication_date>
+                        <news:title>${escapeHtml(data.title)}</news:title>
+                    </news:news>
+                </url>
+            `;
+        });
+
+        let xml = `
+            <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+                ${xmlContents}
+            </urlset>
+        `;
+
+        res.header('Content-Type', 'application/xml');
+        return res.send(xml);
+    })
+    .catch(next);
+};
