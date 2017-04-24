2684496c2b8d85343b3947b93966968cf3784ca0
WebberWu <bird23074035@gmail.com>
2017-04-24 13:34:39 +0800

    硬上 swf

--- mobile_web/controllers/files/index.js
+++ mobile_web/controllers/files/index.js
@@ -2,8 +2,12 @@ import express from 'express';
 let router = express.Router();
 
 import robots from './robots';
+import video from './video';
 
 router.route('/robots.txt')
     .get(robots);
+
+router.route('/video-js.swf')
+    .get(video);
 
 module.exports = router;
