f29d083d7eded67829c59cb8db9b13a749dcdf83
Webber <bird23074035@gmail.com>
2017-06-13 22:46:09 +0800

    ✨ Feat: 加上專題

---
+++ mobile_web/controllers/news/topic.js
@@ -0,0 +1,42 @@
+import co from 'co';
+import express from 'express';
+import getApi from '../../util/getApi';
+import getV4Api from '../../util/getV4Api';
+let router = express.Router();
+const debug = require('debug')('NOWmobile:controllers:news:topic');
+
+module.exports = (req, res, next) => {
+    let { topicId } = req.params;
+
+    co(function*() {
+        let live = yield getApi('kmt/chairman2017');
+
+        let { specialTopics } = yield getV4Api('specialtopics');
+
+        _.map(specialTopics, (topic) => {
+            if(topic.url.indexOf('http') < 0){
+                topic.url = '/news/' + topic.url.split('/').pop();
+            }
+            return topic;
+        });
+
+        let { isOpen } = req.query;
+        let channelName = '專題';
+        let data = {
+            nativeAds: [],
+            specialTopics,
+            channelName,
+            topicId,
+            isOpen,
+            live
+        };
+
+        if(req.query.data === 'PLAYJJ'){
+            return res.json({ data });
+        }
+
+        return res.render('news/topic', data);
+
+    }).catch(next);
+
+};
