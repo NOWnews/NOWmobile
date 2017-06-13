859591ad0bd7c2d89c1276d9e62a23ec4a7e4cfd
Webber <bird23074035@gmail.com>
2017-06-13 11:56:13 +0800

    ✨ Feat: adjust one news

--- mobile_web/controllers/news/one.js
+++ mobile_web/controllers/news/one.js
@@ -1,6 +1,7 @@
 import co from 'co';
 import express from 'express';
 import getApi from '../../util/getApi';
+import getV4Api from '../../util/getV4Api';
 
 let router = express.Router();
 
@@ -17,21 +18,25 @@ module.exports = (req, res, next) => {
 
         let live = yield getApi('kmt/chairman2017');
 
-        let news = yield getApi(`news/${newsId}`);
+        let news = yield getV4Api(`news/${newsId}`);
+
+        let nextandprev = yield getV4Api(`news/${newsId}/nextandprev`);
+
+        let refNews = yield getV4Api(`hot/${news.MainMenu.categoryName}`);
 
         debug('news = %j', news);
 
         // 過濾關鍵字
-        if (news.keywords && news.keywords.length > 0) {
-            let byeText = ['首頁', '速報', '八卦', '政治', 'google編輯嚴選', 'yahoo名人娛樂', '香港雅虎', '電影', '色區', '生活看板', '社會看板', '國際看板' ,'大陸看板' ,'花生網', '旗艦報', '娛樂報', '花生新鮮事', '花生新鮮事旅遊', '地方', '旅遊看板', '科技看板', '要聞', '花生熱話題', '名人時尚看板', '大咖', '娛樂看板', '娛樂', '電視'];
-            let byeTextFormat = ',' + byeText.join('|,') + '|' + byeText.join('|');
-            let myRegExp = new RegExp(byeTextFormat, 'g');
-            news.keywords = news.keywords.replace(myRegExp, '').replace(/^,/, '');
-        }
+        // if (news.keywords && news.keywords.length > 0) {
+        //     let byeText = ['首頁', '速報', '八卦', '政治', 'google編輯嚴選', 'yahoo名人娛樂', '香港雅虎', '電影', '色區', '生活看板', '社會看板', '國際看板' ,'大陸看板' ,'花生網', '旗艦報', '娛樂報', '花生新鮮事', '花生新鮮事旅遊', '地方', '旅遊看板', '科技看板', '要聞', '花生熱話題', '名人時尚看板', '大咖', '娛樂看板', '娛樂', '電視'];
+        //     let byeTextFormat = ',' + byeText.join('|,') + '|' + byeText.join('|');
+        //     let myRegExp = new RegExp(byeTextFormat, 'g');
+        //     news.keywords = news.keywords.replace(myRegExp, '').replace(/^,/, '');
+        // }
 
         let result = yield [
-            getApi(`news/headline`),
-            getApi('category/news')
+            getV4Api(`instant`),
+            getV4Api('menus')
         ];
 
         // record query params
@@ -46,6 +51,13 @@ module.exports = (req, res, next) => {
         let { newsList } = result[0];
 
         let mainCategory = result[1];
+
+        // 加上 上下頁的新聞
+        news.next = nextandprev.next;
+        news.prev = nextandprev.prev;
+
+        // 加上 相關新聞
+        news.refNews = refNews;
 
         let data = {
             news,
