8e84b3b35023ebb516630e160f04a435c370f79b
Webber <bird23074035@gmail.com>
2017-06-28 14:17:32 +0800

    🔨 Refactor: adjust

--- mobile_web/middlewares/redirect.js
+++ mobile_web/middlewares/redirect.js
@@ -8,9 +8,12 @@ module.exports = (app) => {
 
     return (req, res, next) => {
 
-        // 舊的 url 網址格式
-        let newsMatches = req.path.match(/\/n\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/([0-9]+)/);
-        let videoMatches = req.path.match(/\/v\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/([0-9]+)/);
+        // 桌面的 url 網址格式
+        let newsMatches = req.path.match(/\/news\/([0-9]{8})\/([0-9]+)/);
+        // 桌面的分類頁
+        let categoryMatchs = req.path.match(/\/cat\/([0-9.\-A-Za-z]+)/);
+        // 桌面的特輯頁
+        let channelMatchs = req.path.match(/^\/channel\/([0-9]+)/);
 
         let queryString = '';
         if(req.query && req.query.from) {
@@ -21,20 +24,28 @@ module.exports = (app) => {
             queryString = `?utm_source=${req.query.utm_source}&utm_medium=${req.query.utm_medium}&utm_campaign=${req.query.utm_campaign}`;
         }
 
-        // 有符合到舊的新聞內頁格式就直接 redirect
+        // 有符合桌面版新聞內頁格式就直接 redirect
         if(newsMatches !== null && newsMatches.length > 0) {
             let originUrl = newsMatches[0];
-            let nodeId = newsMatches[4];
+            let nodeId = newsMatches[2];
             console.log(chalk.blue.bold(`原網址 "${originUrl}" -------> 導轉 "/news/${nodeId}"`));
             return res.redirect(`/news/${nodeId}${queryString}`);
         }
 
-        // 有符合到舊的影音內頁格式就直接 redirect
-        if(videoMatches !== null && videoMatches.length > 0) {
-            let originUrl = videoMatches[0];
-            let nodeId = videoMatches[4];
-            console.log(chalk.blue.bold(`原網址 "${originUrl}" -------> 導轉 "/video/${nodeId}"`));
-            return res.redirect(`/video/${nodeId}${queryString}`);
+        // 有符合桌面版分類格式就直接 redirect
+        if(categoryMatchs !== null && categoryMatchs.length > 0) {
+            let originUrl = categoryMatchs[0];
+            let categoryName = categoryMatchs[1];
+            console.log(chalk.blue.bold(`原網址 "${originUrl}" -------> 導轉 "/news/category/${categoryName}"`));
+            return res.redirect(`/news/category/${categoryName}${queryString}`);
+        }
+
+        // 有符合桌面版特輯格式就直接 redirect
+        if(channelMatchs !== null && channelMatchs.length > 0) {
+            let originUrl = channelMatchs[0];
+            let channelId = channelMatchs[1];
+            console.log(chalk.blue.bold(`原網址 "${originUrl}" -------> 導轉 "/news/channel/${channelId}"`));
+            return res.redirect(`/news/channel/${channelId}${queryString}`);
         }
 
         return next();
