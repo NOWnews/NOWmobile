af116f5687b82eb64a97b48836049c3ed9400c7a
Webber <bird23074035@gmail.com>
2017-07-05 13:32:24 +0800

    ✨ Feat: 加一個對 ad2004 的 api

---
+++ mobile_web/util/getAdsApi.js
@@ -0,0 +1,34 @@
+
+import fetch from 'node-fetch';
+import iconv from 'iconv-lite';
+const debug = require('debug')('NOWmobile:util:getAdsApi');
+
+module.exports = (ownerId) => {
+    let fetchUrl = `http://ad1.nownews.com/ads.php?ownerid=${ownerId}`;
+
+    // 如果 url 有中文字，建議 encode 會比較沒有問題
+    fetchUrl = encodeURI(fetchUrl);
+
+    debug('Url = ', fetchUrl);
+
+    return fetch(fetchUrl, {
+            timeout: 5000
+        })
+        .then((res) => res.buffer())
+        .then((buffer) => iconv.decode(buffer, 'big5'))
+        .then((ad) => {
+
+            // 如果廣告關閉
+            if (ad === '') {
+                return Promise.resolve({err: '沒有廣告'});
+            }
+
+            // 如果廣告沒有其他東西
+            if (ad.indexOf('"title"') < 0 && ad.indexOf('"img"') < 0 && ad.indexOf('"url"') < 0) {
+                return Promise.resolve({err: '廣告格式錯誤'});
+            }
+
+            return Promise.resolve(JSON.parse(ad));
+
+        });
+};
