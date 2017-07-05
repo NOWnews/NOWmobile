561b32ca723452d8d23792cb055558c6825662bf
Webber <bird23074035@gmail.com>
2017-07-05 18:40:26 +0800

    ✨ Feat: 先用這個方法吧 乾

--- mobile_web/controllers/news/search.js
+++ mobile_web/controllers/news/search.js
@@ -1,6 +1,7 @@
 
 import co from 'co';
 import getV4Api from '../../util/getV4Api';
+import getAdsApi from '../../util/getAdsApi';
 
 const debug = require('debug')('NOWmobile:controllers:news:search');
 
@@ -18,6 +19,32 @@ module.exports = (req, res, next) => {
 
         let mainCategory = result[0];
         let { newsList, ads } = result[1];
+
+        // TODO ---- 廣告先暫時這樣處理 乾
+        ads = [
+            {
+                sn: 1,
+                ad: yield getAdsApi('2995')
+            },{
+                sn: 2,
+                ad: yield getAdsApi('2996')
+            },{
+                sn: 3,
+                ad: yield getAdsApi('2997')
+            },{
+                sn: 4,
+                ad: yield getAdsApi('2998')
+            },{
+                sn: 5,
+                ad: yield getAdsApi('2999')
+            },{
+                sn: 6,
+                ad: yield getAdsApi('3000')
+            },{
+                sn: 7,
+                ad: yield getAdsApi('3001')
+            }];
+        // ------------------------
 
         if(req.query.data === 'PLAYJJ'){
             return res.json({ newsList });
