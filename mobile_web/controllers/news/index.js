9e1b076a5b75a4f16130bd375fcfc7ba17816915
Webber <bird23074035@gmail.com>
2017-07-10 19:33:00 +0800

    🔨 Refactor: 加上註解 刪掉無用之物

--- mobile_web/controllers/news/index.js
+++ mobile_web/controllers/news/index.js
@@ -7,29 +7,32 @@ import topic from './topic';
 import listByCategory from './listByCategory';
 import one from './one';
 import search from './search';
-import special from './special';
 
-router.route('/news/special/:specialType')
-    .get(special);
-
+// 專題頁
 router.route('/news/channel')
     .get(channel);
 
+// 特輯分類頁
 router.route('/news/channel/:channelId')
     .get(channel);
 
+// 專題頁
 router.route('/news/topic')
     .get(topic);
 
+// 專題頁
 router.route('/news/topic/:topicId')
     .get(topic);
 
+// 分類頁
 router.route('/news/category/:taxId')
     .get(listByCategory);
 
+// 新聞搜尋
 router.route('/news/search')
     .get(search);
 
+// 內頁 ( 包含影音，照片 )
 router.route('/news/:newsId')
     .get(one);
 
