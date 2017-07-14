41f69e8ac31aaca19ed5ca8298578fde7d23fcb0
Webber <bird23074035@gmail.com>
2017-07-14 19:20:46 +0800

    🐛 Fix: add 404 status

--- mobile_web/controllers/index.js
+++ mobile_web/controllers/index.js
@@ -24,7 +24,7 @@ module.exports = function(app) {
 
     // 剩下的導去 404 頁面
     app.use('*', function(req, res, next){
-        return res.status(400).render('error/404');
+        return res.status(404).render('error/404');
     });
 
     return function(req, res, next) {
