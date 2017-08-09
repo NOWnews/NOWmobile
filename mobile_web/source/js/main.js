f9a60a8ba25b2278b4957d8e673798917558bf96
Webber <bird23074035@gmail.com>
2017-08-09 19:41:45 +0800

    ✨ Feat: ui 往上滑

--- mobile_web/source/js/main.js
+++ mobile_web/source/js/main.js
@@ -310,4 +310,46 @@ $(function() {
         arrowBtns[0].addEventListener('touchend', arrowEndHandler, false);
         arrowBtns[1].addEventListener('touchend', arrowEndHandler, false);
     }
+
+    // UI 往上滑
+    var slideToTop = $("<div />");
+    slideToTop.html('<i class="fa fa-chevron-up"></i>');
+    slideToTop.css({
+        position: 'fixed',
+        bottom: '20px',
+        right: '25px',
+        width: '40px',
+        height: '40px',
+        color: '#eee',
+        'font-size': '',
+        'line-height': '40px',
+        'text-align': 'center',
+        'background-color': '#222d32',
+        cursor: 'pointer',
+        'border-radius': '5px',
+        'z-index': '99999',
+        opacity: '.7',
+        'display': 'none'
+    });
+    slideToTop.on('mouseenter', function() {
+        $(this).css('opacity', '1');
+    });
+    slideToTop.on('mouseout', function() {
+        $(this).css('opacity', '.7');
+    });
+    $('body').append(slideToTop);
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= 150) {
+            if (!$(slideToTop).is(':visible')) {
+                $(slideToTop).fadeIn(500);
+            }
+        } else {
+            $(slideToTop).fadeOut(500);
+        }
+    });
+    $(slideToTop).click(function() {
+        $("body").animate({
+            scrollTop: 0
+        }, 500);
+    });
 });
