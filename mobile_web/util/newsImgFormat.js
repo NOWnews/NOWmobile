ab0ad7f2d16e540983625199a2a9d73712de728b
Webber <bird23074035@gmail.com>
2017-08-11 10:35:42 +0800

    ✨ Feat: update

--- mobile_web/util/newsImgFormat.js
+++ mobile_web/util/newsImgFormat.js
@@ -7,13 +7,13 @@ module.exports = (news, isList) => {
     if (isList) {
         news = _.map(news, (item) => {
             // item.formatImg = item.MainPhoto.googleCDN || item.MainPhoto.thumbnail || 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg';
-            item.formatImg = item.MainPhoto.url ? 'https://imgapiv2.nownews.com/?w=640&h=360&q=70&src=' + item.MainPhoto.url : 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg';
+            item.formatImg = item.MainPhoto ? 'https://imgapiv2.nownews.com/?w=640&h=360&q=70&src=' + item.MainPhoto.url : 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg';
             return item;
         });
         return news;
 
     } else {
-        news.formatImg = news.MainPhoto.googleCDN || news.MainPhoto.thumbnail || 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg';
+        news.formatImg = news.MainPhoto ? news.MainPhoto.googleCDN || news.MainPhoto.thumbnail || 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg' : 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg' ;
         return news;
     }
 
