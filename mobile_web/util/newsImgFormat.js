811275192f4407626faf1cbfe03d940c23413572
Webber <bird23074035@gmail.com>
2017-09-11 11:54:21 +0800

    🔨 Refactor: 更新 img foramt 的邏輯

--- mobile_web/util/newsImgFormat.js
+++ mobile_web/util/newsImgFormat.js
@@ -2,19 +2,52 @@
 import _ from 'lodash';
 const debug = require('debug')('NOWmobile:util:newsImgFormat');
 
-module.exports = (news, isList) => {
+module.exports = (news, isList, size = 'large') => {
+    const defaultImgUrl = 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg';
+    const imgapi = config.get('imgApi.host');
+    const listSize = {
+        large: `${imgapi}?w=640&q=70&src=`,
+        thumbnail: `${imgapi}?w=300&q=90&src=`
+    };
+    const oneSize = `${imgapi}?w=640&q=70&src=`;
+    const imgRegexString = /^(http|https):\/\/(s|img|legacy).nownews.com\//;
+    let checkImgDomain = (url) => {
+
+    };
 
     if (isList) {
         news = _.map(news, (item) => {
-            // item.formatImg = item.MainPhoto.googleCDN || item.MainPhoto.thumbnail || 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg';
-            item.formatImg = item.MainPhoto ? item.MainPhoto.url : 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg';
+
+            if (!item.MainPhoto) {
+                item.MainPhoto = {
+                    formatImg: `${listSize[size]}${defaultImgUrl}`,
+                    originalImg: defaultImgUrl
+                };
+                return item;
+            }
+
+            if (size ===  'large') {
+                item.MainPhoto.formatImg = item.MainPhoto.sizeFormat.w640q70;
+            } else {
+                item.MainPhoto.formatImg = item.MainPhoto.sizeFormat.w300q70;
+            }
+
+            item.MainPhoto.originalImg = item.MainPhoto.url
+
             return item;
         });
-        return news;
-
     } else {
-        news.formatImg = news.MainPhoto ? news.MainPhoto.googleCDN || news.MainPhoto.thumbnail || 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg' : 'https://legacy.nownews.com/NOWnews_default/default_terry.jpg' ;
-        return news;
+        if (!news.MainPhoto) {
+            news.MainPhoto = {
+                formatImg: `${listSize[size]}${defaultImgUrl}`,
+                originalImg: defaultImgUrl
+            };
+            return news;
+        }
+        news.MainPhoto.formatImg = news.MainPhoto.sizeFormat.w640q70;
+        news.MainPhoto.originalImg = news.MainPhoto.url;
     }
+
+    return news;
 
 };
