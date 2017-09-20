20ecd534c73e4ebf427f4cbfe0c3a80095784a01
JaosnHsieh <appleoxxo@gmail.com>
2017-09-20 14:59:13 +0800

    🔨 Refactor:

--- mobile_web/controllers/news/one.js
+++ mobile_web/controllers/news/one.js
@@ -19,7 +19,7 @@ module.exports = (req, res, next) => {
         let news = yield getV4Api(`news/${newsId}`);
 
         if( news.statusCode && news.statusCode !== 200 ){
-             throw new Error(`news/${newsId}找不到新聞!!`);;
+             throw new Error(`news/${newsId}找不到新聞!!`);
         }
 
         let live = yield getV4Api('live/info');
