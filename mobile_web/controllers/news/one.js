c7c07da5358bce47e4f14385019f7a51f82e58e0
esbb48 <esbb48@gmail.com>
2017-09-25 11:23:13 +0800

    🐛 Fix: Prevent Cannot read property \'formatImg\' of null

--- mobile_web/controllers/news/one.js
+++ mobile_web/controllers/news/one.js
@@ -56,13 +56,14 @@ module.exports = (req, res, next) => {
         // 加上 相關新聞
         news.refNews = refNews;
 
-        // 加上 jsonld
-        news.jsonld = jsonld(news);
-
         news = newsImgFormat(news);
         news.headline = newsImgFormat(news.headline, true, 'thumbnail');
         news.refNews = newsImgFormat(news.refNews, true, 'thumbnail');
         newsList = newsImgFormat(newsList, true, 'thumbnail');
+
+        // 加上 jsonld 
+        news.jsonld = jsonld(news);
+
         let isOnePage = true;
         let data = {
             news,
