9e376ccd16f97d1fbfedb82b1092ac45c81fd0c6
esbb48 <esbb48@gmail.com>
2017-09-29 22:12:29 +0800

    ✨ Feat: Update petsmao

--- mobile_web/controllers/subweb/list.js
+++ mobile_web/controllers/subweb/list.js
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
           { img: 'bobee', text: '保庇Bobee', url: 'https://bobee.nownews.com/' },
           { img: 'ifunnow', text: 'iFunNOW', url: 'https://ifunnow.nownews.com/' },
           { img: 'playnow', text: 'PlayNOW', url: 'https://playnow.nownews.com/' },
-          { img: 'petsmao', text: '寵毛網', url: 'https://www.petsmao.com/' },
+          { img: 'petsmao', text: '寵毛網', url: 'https://petsmao.nownews.com/' },
           { img: 'sport', text: 'Sport', url: 'https://sport.nownews.com/' }
         ];
 
