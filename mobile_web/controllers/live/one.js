85e3dc2a9a1ec3fd30a008e7d6b832d61fd87e69
wayne1025 <wayne1025.tw@hotmail.com>
2018-01-24 11:01:23 +0800

    修改回來判斷區域性給網頁

--- mobile_web/controllers/live/one.js
+++ mobile_web/controllers/live/one.js
@@ -51,8 +51,7 @@ module.exports = async (req, res, next) => {
         }
 
         // 測試用 ---------------------------------------------------------------
-        // if (geo && geo.country === 'CN') {
-        if(req.query.data === 'CN'){
+        if (geo && geo.country === 'CN') {
             data.live.title = chineseConv.sify(data.live.title);
             data.videoList = _.map(data.videoList, (video) => {
                 video.title = chineseConv.sify(video.title);
@@ -71,7 +70,7 @@ module.exports = async (req, res, next) => {
         let dirtyIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress || "";
         let ip = dirtyIp.split(',').shift();
         let geo = geoip.lookup(ip);
-        if (req.query.data === 'CNLive') {
+        if (geo && geo.country === 'CN') {
             data.live.title = chineseConv.sify(data.live.title);
             data.videoList = _.map(data.videoList, (video) => {
                 video.title = chineseConv.sify(video.title);
@@ -79,6 +78,7 @@ module.exports = async (req, res, next) => {
             });
             data.news.refNews = _.map(data.news.refNews, (news) => {
                 news.title = chineseConv.sify(news.title);
+                //目前拿到的沒有name
                 // news.category.name = chineseConv.sify(news.category.name);
                 return news;
             });
