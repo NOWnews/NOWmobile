948f3a192d1df902febbb913cc00e907033e4855
LiweiTW <Vincent.cdw@gmail.com>
2018-05-08 12:56:25 +0800

    no message

--- mobile_web/files/firebase-messaging-sw.js
+++ mobile_web/files/firebase-messaging-sw.js
@@ -15,5 +15,6 @@ function getUpdateTime(now)
 }
 
 self.SENDER_ID = '';
-// importScripts('https://www.likr.com.tw/pushEndPoint/js/sw_fcm_import.js?' + getUpdateTime(0));
+
+importScripts('//pwa.js');
 
