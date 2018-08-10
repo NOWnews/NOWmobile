c0b51090d94d368ff8b0133c058297888bfdcc98
LiweiTW <Vincent.cdw@gmail.com>
2018-08-10 14:32:58 +0800

    避免 New Beanfun 出現蓋板

--- mobile_web/source/js/main.js
+++ mobile_web/source/js/main.js
@@ -71,7 +71,9 @@ $(function() {
             $('.ads-cover .close').on('click', closeAdsCover);
         };
 
-        if (count === 1 || count === 3 || count === 5 || count === 7 ) {
+            var userAgent = navigator.userAgent;
+            var beangoRe = /BeanGo/;
+            if ((count === 1 || count === 3 || count === 5 || count === 7) && (!beangoRe.test(userAgent))) {
             $window.load(function() {
                 var hasDFP = $('.ads-cover.dfp > div').css('display') === 'none' ? false : true;
                 // 如果沒有 dfp 廣告就塞入成果的廣告碼
